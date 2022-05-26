239a8d1749db1878fa6c7fb5fd165bc027b4fdbf
movieonlyemail4 <movie.only.email4@gmail.com>
2022-05-27 03:03:02 +0800

    add comment

--- index.php
+++ index.php
@@ -32,7 +32,11 @@ function tukarmatrix($array_asal, $groupkan)
         return $array_final;
     }
     
+    // change this array to desired value
     $val_negeri = array(2,4,3,5,1,6,8,4,5,2,3,5,1,2,3,5,7,5,4,6,7,4,3,6,4,5,6,7,2,3,4,6,7,4,3,5,4,6);
+    
+    // call the function
+    // second parameter is for the size of matrix, see README
     $new_aarara = tukarmatrix($val_negeri, 4);
     
     foreach($new_aarara as $val){
